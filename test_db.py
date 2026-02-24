d376b72d2e862be7f82f79ca65245a228bd8e1fd
jaydonos <168603719+jaydonos@users.noreply.github.com>
2026-02-24 13:32:30 -0700

    Test sql python code for database

---
+++ test_db.py
@@ -0,0 +1,33 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host = "localhost",
+    user = "botuser",
+    password = "password",
+    database = "discord_bot"
+)
+
+cur.conn.cursor()
+
+#Create table:
+cur.execute("""
+CREATE TABLE IF NOT EXISTS users (
+  user_id VARCHAR(50) PRIMARY KEY,
+  balance INT NOT NULL
+)
+""")
+
+#Insert 
+cur.execute("""
+INSERT INTO users (user_id, balance)
+VALUES (%s, %s)
+ON DUPLICATE KEY UPDATE balance = VALUES(balance)
+""", ("test_user", 1000))
+
+cur.execute("SELECT user_id, balance FROM users WHERE user_id = %s", ("test_user",))
+row = cur.fetchone()
+
+print("Fetched from DB:", row)
+
+cur.close()
+conn.close()
